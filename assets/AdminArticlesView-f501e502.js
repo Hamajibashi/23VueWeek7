5f5811dd9d3ef0fbf00a6aa432d05620937f40f2
はまじ <86872438+Hamajibashi@users.noreply.github.com>
2023-03-10 14:31:40 +0800

    deploy

---
+++ assets/AdminArticlesView-f501e502.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-1731e299.js";const s={},t={class:"h2 fw-bold my-4"};function n(r,_){return c(),o("h1",t,"貼文管理")}const f=e(s,[["render",n]]);export{f as default};
---
+++ assets/AdminCouponsView-a468c80f.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-1731e299.js";const s={},n={class:"h2 fw-bold my-4"};function t(r,_){return o(),c("h1",n,"優惠券管理")}const f=e(s,[["render",t]]);export{f as default};
